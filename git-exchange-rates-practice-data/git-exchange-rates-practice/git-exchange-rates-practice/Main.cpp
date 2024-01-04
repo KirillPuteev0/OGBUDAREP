c518cd97328bb615ccc0dffa4bf2b27f5af414f8
KirillPuteev0 <sorrokomail@gmail.com>
2024-01-04 15:53:25 +0300

    3 ЧАС СИЖУ

--- git-exchange-rates-practice-data/git-exchange-rates-practice/git-exchange-rates-practice/Main.cpp
+++ git-exchange-rates-practice-data/git-exchange-rates-practice/git-exchange-rates-practice/Main.cpp
@@ -6,6 +6,6 @@ int main()
 {
     cout << "Laboratory work #4. GIT\n";
     cout << "Variant #4. git-exchange-rates-practice\n";
-    cout << "Author: Puteev Kirill\n";
+    cout << "Author: Yana Iluchenko\n";
     return 0;
 }
